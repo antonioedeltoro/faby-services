a0cd4f82a3060755f593cb3851523a5b5cead86b
Antonio del Toro <antonioedeltoro@gmail.com>
2025-07-02 13:50:50 -0600

    .

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,22 +1,20 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+
+const { ADMIN_EMAIL, ADMIN_PASSWORD } = process.env;
 
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
 
   console.log("🔐 Received:", email, password);
-  console.log(
-    "✅ Expected:",
-    process.env.ADMIN_EMAIL,
-    process.env.ADMIN_PASSWORD
-  );
+  console.log("✅ Expected:", ADMIN_EMAIL, ADMIN_PASSWORD);
 
-  if (
-    email === process.env.ADMIN_EMAIL &&
-    password === process.env.ADMIN_PASSWORD
-  ) {
-    return res.status(200).json({ token: "fake-jwt-token" });
+  if (email === ADMIN_EMAIL && password === ADMIN_PASSWORD) {
+    const token = jwt.sign({ email }, "your_jwt_secret", { expiresIn: "1h" });
+    return res.json({ token });
   } else {
-    return res.status(403).json({ message: "Invalid email or password" });
+    return res.status(403).json({ message: "Forbidden" });
   }
 });
 
