78e7f8c8da0db50dfacfa76e8c75430a8c1f44c6
Antonio del Toro <antonioedeltoro@gmail.com>
2025-07-02 14:50:51 -0600

    install CORS

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // Optional: if you're sending cookies/auth headers
+  })
+);
 
 // Routes
 const newsRoutes = require("./routes/news");
