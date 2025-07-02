8a8e8500538a4fb01e8dcabc30cf82ec21098ab8
Antonio del Toro <antonioedeltoro@gmail.com>
2025-07-02 14:54:55 -0600

    cors

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true, // Optional: if you're sending cookies/auth headers
+    origin: "http://localhost:5173", // Your Vite dev URL
+    credentials: true, // Only if you're using cookies or auth headers
   })
 );
 
